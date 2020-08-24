30ff4fbf8c2b5383ac8c60f054e6443711b0ab87
Slava Isaev <slava.isaev@gmail.com>
2020-08-24 17:17:58 +0000

    Adjausting certificates path to workflow.

--- scripts/create-self-cert.sh
+++ scripts/create-self-cert.sh
@@ -3,7 +3,7 @@
 # Creates SSL certificate and private key
 # The key.pem and cert.pem outputs are stored in the TARGET directory.
 
-export TARGET=../certs
+export TARGET=./certs
 mkdir -p $TARGET
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 \
     -subj "/C=SI/ST=SI/L=Ljubljana/O=Nil/CN=crm.nil.com" \
