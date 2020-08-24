051120d21dce07fa97d43d5d21c4a13d5949bae4
Slava Isaev <slava.isaev@gmail.com>
2020-08-24 18:18:55 +0000

    Adding a script to perform all tests.

---
+++ tests/full_test_scenario.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## Configuring environment
+pip install -r requires.txt
+
+## Static verification
+#pylint *.py
+bandit *.py --skip B101
+
+## Testing
+pytest --verbose src/test_db.py
+pytest --verbose src/test_orm.py
+
+## Creating setup
+scripts/create-self-cert.sh
+docker-compose down --volumes
+docker-compose up --build -d
+./tests/wait_for_url.sh 60 https://localhost:8050
+
+## System tests
+pytest tests/test_system.py
